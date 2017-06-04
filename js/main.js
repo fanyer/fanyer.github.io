52da021a9e73cca969085f998a83d0d7421ffa6e
fanyer <iofanyer@gmail.com>
2017-06-05 05:50:58 +0800

    update controller

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       $('.panel-cover').addClass('panel-cover--collapsed');
     } else {
       $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '700px', 'width': '30%'}, 400, swing = 'swing', function() {} );
+      $('.panel-cover').animate({'max-width': '700px', 'width': '25%'}, 400, swing = 'swing', function() {} );
     }
   });
 
